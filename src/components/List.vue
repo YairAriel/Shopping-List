dbfceec0c434e9ebff0280ecd6f50a05f3ac54f3
Yair Ariel <yair009@gmail.com>
2019-02-20 22:14:52 +0200

    feature - change progress bar color according to the progress

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <Spinner size="40" :line-size="8" line-fg-color="#455A64" v-show="loading" 
+        <Spinner size="60" :line-size="8" line-fg-color="#455A64" v-show="loading" 
         class="spinner" message="Loading..."></Spinner>
         <div v-if="listOfItems.length > 0">
-            <v-progress-linear color="info" height="10" :value="percentage"></v-progress-linear>
+            <v-progress-linear :color="barColor" height="10" :value="percentage" class="mb-3"></v-progress-linear>
             <v-list :class="mobile">
                 <Item :item.sync="data" v-for="(data, index) in listOfItems" :key="index" :itemIndex="index"
                     @checked="doneCounter++" @unchecked="doneCounter--"/>
@@ -36,6 +36,21 @@
             },
             percentage () {
                 return Math.floor((this.doneCounter / this.listOfItems.length) * 100);
+            },
+            barColor () {
+                if (this.percentage == 0) {
+                    return '#607D8B';
+                } else if (this.percentage < 20) {
+                    return '#B71C1C';
+                } else if (this.percentage < 40) {
+                    return '#F44336';
+                } else if (this.percentage < 60) {
+                    return '#FF9800';
+                } else if (this.percentage < 80) {
+                    return '#FFEB3B';
+                } else {
+                    return '#00E676';
+                }
             }
         },
         created() {
